0d623e7725ddf2b6777467c6dfe375edf5bd1341
Ahmed Teaima <phabosamra2055@gmail.com>
2021-07-07 10:44:14 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Home from "./pages/Home.jsx";
+import SearchPage from "./pages/SearchPage.jsx";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/keys.js
@@ -0,0 +1,5 @@
+export const API_KEY = "AIzaSyCkhiEYDjmIBWGm69Vk1z5rOTOTNipd9eU";
+
+export default API_KEY;
+
+// https://developers.google.com/custom-search/v1/using_rest
